3f0ae0a60006d5e844464c2989c8bb73db46ff88
yshashanky <812shanky@gmail.com>
2022-07-16 16:50:01 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+let count = 0;
+
 const userAction = async () => {
   const response = await fetch("https://api.adviceslip.com/advice");
-  const myJson = await response.json(); //extract JSON from the http response
-
+  const myJson = await response.json();
   return myJson.slip.advice;
 };
 
 const getAdvice = () => {
+  count = count + 1;
+  document.getElementById("count").innerHTML = count;
   userAction().then(
     (myJson) => (document.getElementById("quote").innerHTML = myJson)
   );
