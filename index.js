172c031c9df1dd12903e57700564827c19eab79f
yshashanky <812shanky@gmail.com>
2022-07-18 23:43:27 +0530

    update

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 let count = 0;
 
-const userAction = async () => {
-  const response = await fetch("https://api.adviceslip.com/advice");
-  const myJson = await response.json();
-  return myJson.slip.advice;
+const getAdvice = () => {
+  userAction();
 };
 
-const getAdvice = () => {
+const userAction = async () => {
   count = count + 1;
-  document.getElementById("count").innerHTML = count;
-  userAction().then(
-    (myJson) => (document.getElementById("quote").innerHTML = myJson)
-  );
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const myJson = await response.json();
+    document.getElementById("count").innerHTML = count;
+    document.getElementById("quote").innerHTML = myJson.slip.advice;
+    return 1;
+  } catch (err) {
+    count = count - 1;
+    document.getElementById("count").innerHTML = count;
+    document.getElementById("quote").innerHTML = "Please try after sometime!!";
+  }
 };
 
 window.onload = function () {
   var reloading = (document.getElementById("quote").innerHTML =
     "Please wait for the advice!!");
+  document.getElementById("count").innerHTML = 1;
   if (reloading) {
-    getAdvice();
+    userAction();
   }
 };
